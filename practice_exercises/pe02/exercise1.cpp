53ec0440cb8db971e9af5e5ed1f35e4b09718f4e
Conrad <conrad.heidebrecht@gmail.com>
2019-02-25 15:41:35 -0500

    reformatted structure

---
+++ practice_exercises/pe02/exercise1.cpp
@@ -0,0 +1,12 @@
+struct TwinPair {
+    int val1, val2;
+    TwinPair() : val1(0), val2(0) {}
+};
+
+bool is_prime(unsigned int n) {
+    
+}
+
+TwinPair find_twin_primes(unsigned int chosen_index) {
+
+}
